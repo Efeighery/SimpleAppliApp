00dbddfedee81ba507627b7f3f16d6145c413a26
Efeighery <eoghanrcfeighery@gmail.com>
2023-03-09 23:38:39 +0000

    CD

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+sudo apt update && sudo apt install nodejs npm
+
+sudo npm install -g pm2
+
+pm2 stop example_app
+
+cd SimpleApplication/
+
+npm install
+
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+
+pm2 start ./bin/www --name example_app
